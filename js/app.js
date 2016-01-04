6c197530a46f8f479716ce87a2b775bdd0c7e38e
catalinka.28@gmail.com <catalinka.28@gmail.com>
2016-01-04 13:36:02 +0100

    main template with menu done

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Kasia on 2015-12-31.
+ */
+$(document).ready(function(){
+
+    //var menu = $(".menu");
+    //var menuStartPosition = menu.offset().top;
+    //var spacer = $("#menuSpacer");
+    //
+    //$(window).on("scroll", function(event){
+    //
+    //    if($(window).scrollTop() > menuStartPosition){
+    //        menu.addClass("sticky");
+    //        spacer.height(menu.height());
+    //    }else {
+    //        menu.removeClass("sticky");
+    //        spacer.height(0);
+    //    }
+    //});
+    //
+    //
+    //function setMenu() {
+    //    $(window).on('resize', function() {
+    //        if ($(window).width() < 735) {
+    //            $('.nav').hide();
+    //            $('.menuBtn').show();
+    //        } else {
+    //            $('.nav').show();
+    //            $('.menuBtn').hide();
+    //            $('.mobileMenu').hide();
+    //        }
+    //    });
+    //}
+    //
+    //function setClick() {
+    //    $('.menuBtn').on('click', function() {
+    //        $('.mobileMenu').toggle();
+    //    });
+    //}
+    //
+    ////setMenu();
+    //setClick();
+
+
+
+});
